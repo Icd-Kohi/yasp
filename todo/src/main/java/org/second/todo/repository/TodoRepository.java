77dd746ca40c8b76caf9e32048a1c97d02ff8706
Icd-Kohi <luambo13@gmail.com>
2026-07-26 15:07:01 -0300

    add: Todo repository

--- todo/src/main/java/org/second/todo/repository/TodoRepository.java
+++ todo/src/main/java/org/second/todo/repository/TodoRepository.java
@@ -1,4 +1,9 @@
 package org.second.todo.repository;
 
-public interface TodoRepository {
+import org.second.todo.entities.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
