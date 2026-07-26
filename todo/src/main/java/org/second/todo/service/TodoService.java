8d8dd1ba9b3a1b0f8d3c1294cee0daa8c1981839
Icd-Kohi <luambo13@gmail.com>
2026-07-26 15:08:09 -0300

    feat: implement Todo service

--- todo/src/main/java/org/second/todo/service/TodoService.java
+++ todo/src/main/java/org/second/todo/service/TodoService.java
@@ -1,4 +1,40 @@
 package org.second.todo.service;
 
+import org.second.todo.entities.Todo;
+import org.second.todo.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by(Sort.Direction.DESC,"priority")
+                .and(Sort.by(Sort.Direction.ASC,"id"));
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return this.list();
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return this.list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return this.list();
+    }
+
 }
