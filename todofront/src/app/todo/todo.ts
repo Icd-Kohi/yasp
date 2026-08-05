1e86e150544aa7dc7cff36be8647ba52348b48ce
Icd-Kohi <luambo13@gmail.com>
2026-08-05 00:12:30 -0300

    ADD: todo frontend structure

---
+++ todofront/src/app/todo/todo.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  imports: [],
+  templateUrl: './todo.html',
+  styleUrl: './todo.css',
+})
+export class TodoComponent {
+
+  nothing(): void{
+
+  }
+}
